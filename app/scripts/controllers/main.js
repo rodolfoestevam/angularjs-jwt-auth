349ecb08edf97b8ad8bc72214709b099f579e41b
Rodolfo Estevam <rodolfoestevam.a@gmail.com>
2018-05-31 15:18:15 -0300

    release 1.0

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name angularjsJwtAuthApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the angularjsJwtAuthApp
+ */
+angular.module('angularjsJwtAuthApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
