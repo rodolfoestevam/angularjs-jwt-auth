c82be8c8a38816473837b593be5c690e3e36a4b8
Rodolfo Estevam <rodolfoestevam.a@gmail.com>
2018-05-31 15:16:38 -0300

    added ui-router on app.js

---
+++ app/scripts/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name angularjsJwtAuthApp
+ * @description
+ * # angularjsJwtAuthApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('angularjsJwtAuthApp', ['ui.router']);
