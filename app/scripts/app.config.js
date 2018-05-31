a5926ce08eeada07da9b26c78774dd77fc3a3954
Rodolfo Estevam <rodolfoestevam.a@gmail.com>
2018-05-31 15:46:49 -0300

    added controller to the register url

--- app/scripts/app.config.js
+++ app/scripts/app.config.js
@@ -1,14 +1,18 @@
-angular.module('angularjsJwtAuthApp').config(function ($stateProvider) {
+angular.module('angularjsJwtAuthApp').config(function ($urlRouterProvider,
+  $stateProvider) {
+
+  $urlRouterProvider.otherwise('/');
 
   $stateProvider
-  
-  .state('main', {
-    url: '/',
-    templateUrl: '/views/main.html'
-  })
 
-  .state('register', {
-    url: '/register',
-    templateUrl: '/views/register.html'
-  })
+    .state('main', {
+      url: '/',
+      templateUrl: '/views/main.html'
+    })
+
+    .state('register', {
+      url: '/register',
+      templateUrl: '/views/register.html',
+      controller: 'RegisterCtrl'
+    })
 })
